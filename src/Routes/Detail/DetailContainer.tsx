1b78dee474eb61b8910b34c5ff8518b381ff893c
miju-Park <miju462@ncsoft.com>
2020-05-07 10:51:20 +0900

    Detail Container

--- src/Routes/Detail/DetailContainer.tsx
+++ src/Routes/Detail/DetailContainer.tsx
@@ -1,16 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DetailPresenter from "./DetailPresenter";
+import { useParams, useHistory, useLocation } from "react-router-dom";
+import { moviesAPI, tvAPI } from "../../api";
 export interface DetailInterface {
-  result: null;
-  error: null;
+  result: any;
+  error: string;
   loading: boolean;
 }
+interface ParamsInterface {
+  id: string;
+}
 export default function DetailContainer() {
+  const { pathname } = useLocation();
   const [state, setState] = useState({
     result: null,
     loading: true,
-    error: null,
+    error: "",
+    isMovie: pathname.includes("/movie/"),
   });
-  const { result, loading, error } = state;
+  const { result, loading, error, isMovie } = state;
+  const { id } = useParams<ParamsInterface>();
+  const { push } = useHistory();
+
+  useEffect(() => {
+    async function get() {
+      const parsedId = parseInt(id);
+      if (isNaN(parsedId)) {
+        return push("/");
+      }
+      let result = null;
+      try {
+        if (isMovie) {
+          ({ data: result } = await moviesAPI.movieDetail(parsedId));
+        } else {
+          ({ data: result } = await tvAPI.showDetail(parsedId));
+        }
+        setState({ ...state, loading: false, result });
+      } catch (e) {
+        setState({ ...state, error: "Can't find anything", loading: false });
+      }
+    }
+    get();
+  }, []);
+  console.log(state);
   return <DetailPresenter result={result} loading={loading} error={error} />;
 }
