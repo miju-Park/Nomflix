edb3f289624e75792fa4ff2847d216d2214eebf0
miju-Park <miju462@ncsoft.com>
2020-05-22 08:38:18 +0900

    Updates

---
+++ precache-manifest.66f5c74855c6d3bdc99ad30f040023fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3115e8d429a0b489a3ae5deaddb03ad5",
+    "url": "/nomflix/index.html"
+  },
+  {
+    "revision": "880ab77ec4721a78468c",
+    "url": "/nomflix/static/js/2.75c60e1b.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/nomflix/static/js/2.75c60e1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ebdab6ea4f060e91528",
+    "url": "/nomflix/static/js/main.bbef2ec8.chunk.js"
+  },
+  {
+    "revision": "7f0b2571cc398dde6626",
+    "url": "/nomflix/static/js/runtime-main.181cbd9a.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/nomflix/static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
